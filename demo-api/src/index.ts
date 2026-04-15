a9891b522eeb051749d2c1d37b4cc5b81500761d
SeigoOhtani <seigo.otani@persol.co.jp>
2026-04-15 16:38:34 +0900

    Azureでも動くように修正

--- demo-api/src/index.ts
+++ demo-api/src/index.ts
@@ -22,7 +22,7 @@ app.route('/', operation)
 
 serve({
   fetch: app.fetch,
-  port: 8000
+  port: Number(process.env.PORT) || 8000,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
